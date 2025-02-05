0d65f70a323d6be49e4768df71807981e795db48
Sukhmeet Singh Oberoi <sukhmeetoberoi@gmail.com>
2025-02-06 03:11:58 +0530

    Add files via upload

---
+++ pattern5.cpp
@@ -0,0 +1,23 @@
+/*  1
+    21
+    321
+    4321*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n ; 
+    cin>>n;
+    int i=1;
+    int count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<i-j+1;
+            count=count+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
